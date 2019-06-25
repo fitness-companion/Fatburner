2e3b31f2f1e1c4fb1492dd7846808ddd74af5ee7
Eric Liu <eric__1234@outlook.com>
2019-06-24 20:24:12 -0400

    Fix merge conflict

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-/**
- * @format
- */
-
-<<<<<<< HEAD
-=======
-"use-strict";
-
->>>>>>> UI changes
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
